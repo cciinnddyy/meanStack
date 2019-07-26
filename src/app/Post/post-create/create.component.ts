fd5679dcf9dfd9bb76ca42e8515095f1b1bf583c
cindy <hsuan@192-168-1-114.tpgi.com.au>
2019-07-26 12:58:02 +1000

    mean stack first class

---
+++ src/app/Post/post-create/create.component.ts
@@ -0,0 +1,16 @@
+
+// to get the decorector feature '@'
+import {Component} from '@angular/core';
+
+@Component({
+
+  selector: 'app-post-create',
+
+  templateUrl: './post-create/create.component.html'
+
+})
+
+export class PostCreateComp {
+
+}
+
